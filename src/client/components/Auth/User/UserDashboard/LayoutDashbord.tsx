373f84b821357a21f545d927693b3ea7ba019dfd
Ola Kuyooro <pabloalabanza9@gmail.com>
2023-12-08 12:09:44 +0100

    updated svgs

--- src/client/components/Auth/User/UserDashboard/LayoutDashbord.tsx
+++ src/client/components/Auth/User/UserDashboard/LayoutDashbord.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import Layout from '../../../Layout';
+import React from "react";
+import Layout from "../../../Layout";
+import User from "@/src/client/shared/Svg/User";
+
 
 const LayoutDashbord = () => {
-	return (
-		<Layout>
-			<div>This user dashboard</div>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <div>
+		<User/>
+		
+      </div>
+    </Layout>
+  );
 };
 
 export default LayoutDashbord;
---
+++ src/client/shared/Svg/Deposit.tsx
@@ -0,0 +1,15 @@
+const Deposit = () => {
+    return ( <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g opacity="0.5">
+    <path d="M10 4.63336H18.3333" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.85 1.66669H16.4833C17.9667 1.66669 18.3333 2.03335 18.3333 3.50002V6.92502C18.3333 8.39169 17.9667 8.75835 16.4833 8.75835H11.85C10.3667 8.75835 10 8.39169 10 6.92502V3.50002C10 2.03335 10.3667 1.66669 11.85 1.66669Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1.6665 14.2167H9.99984" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M3.5165 11.25H8.14984C9.63317 11.25 9.99984 11.6167 9.99984 13.0833V16.5083C9.99984 17.975 9.63317 18.3417 8.14984 18.3417H3.5165C2.03317 18.3417 1.6665 17.975 1.6665 16.5083V13.0833C1.6665 11.6167 2.03317 11.25 3.5165 11.25Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path opacity="0.4" d="M18.3333 12.5C18.3333 15.725 15.725 18.3333 12.5 18.3333L13.375 16.875" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path opacity="0.4" d="M1.6665 7.50002C1.6665 4.27502 4.27484 1.66669 7.49984 1.66669L6.62485 3.12502" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </g>
+    </svg>
+     );
+}
+ 
+export default Deposit;
