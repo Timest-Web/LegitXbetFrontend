64fac85be2339778e140e74662c65252668e21a1
Ushie01 <abugodwinaj@gmail.com>
2023-12-13 07:31:01 +0100

    update:font on betslip

--- src/client/components/LandingPage/MainSection/RightSection/BetSlip/BetTypes/MultipleBet.tsx
+++ src/client/components/LandingPage/MainSection/RightSection/BetSlip/BetTypes/MultipleBet.tsx
@@ -6,10 +6,10 @@ import TagCross from '@/src/client/shared/Svg/TagCross';
 const MultipleBet = () => {
 	return (
 		<div className='flex flex-col items-center justify-center h-full w-ful py-3 px-5'>
-			<div className='flex items-center justify-between font-bold text-sm text-black w-full'>
+			<div className='flex items-center justify-between font-bold text-xs text-black w-full'>
 				<p className='cursor-default'>No.</p>
 
-				<div className='flex items-center space-x-2 cursor-pointer'>
+				<div className='flex items-center space-x-2 cursor-pointer text-xs'>
 					<p className='underline'>Delete</p>
 					<TagCross />
 				</div>
@@ -17,11 +17,11 @@ const MultipleBet = () => {
 
 			<div className='flex flex-col space-y-6 w-full pb-16 pt-6'>
 				<div className='flex  items-start space-x-7 w-full text-sm text-black'>
-					<div className='flex items-start justify-between w-full'>
+					<div className='flex items-start justify-between w-full text-xs'>
 						<p>1</p>
 						<p>Man City</p>
 						<p>v</p>
-						<div className='flex flex-col space-y-3'>
+						<div className='flex flex-col space-y-3 text-xs'>
 							<p>Chelsea</p>
 							<p className='text-sm font-bold'>Draw 2.23</p>
 						</div>
@@ -29,9 +29,9 @@ const MultipleBet = () => {
 					<DeleteIcon />
 				</div>
 				<div className='flex  items-start space-x-7 w-full text-sm text-black'>
-					<div className='flex items-start justify-between w-full'>
+					<div className='flex items-start justify-between w-full text-xs'>
 						<p>2</p>
-						<div className='flex flex-col space-y-3'>
+						<div className='flex flex-col space-y-3 text-xs'>
 							<p>Real Madrid</p>
 							<p className='text-sm font-bold'>Win 1.14</p>
 						</div>
