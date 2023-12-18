98c1e982a1e1570ef3fa7365b7a3c8b4f146c75b
Ushie01 <abugodwinaj@gmail.com>
2023-12-18 11:33:08 +0100

    update: customer title name

--- src/client/shared/Drawer/CustomerCareDrawer.tsx
+++ src/client/shared/Drawer/CustomerCareDrawer.tsx
@@ -25,7 +25,7 @@ const CustomerCareModal = ({
 						className={`flex flex-col items-center py-5 z-10000 fixed bg-white right-0 h-[600px] bottom-0 w-2/6 transform rounded-2xl ${className}`}>
 						<div className='flex items-center justify-between w-full px-8'>
 							<p className='text-black font-bold'>
-								Let X bet Customer support
+								Legit X bet Customer support
 							</p>
 							<button
 								className='flex items-center justify-center px-4 py-1 text-center text-xs border border-gray-600 rounded'
