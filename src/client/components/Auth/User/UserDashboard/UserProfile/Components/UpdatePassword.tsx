f498f66c3e2dcd353337cdf51ea70e4c3f90ef90
Ola Kuyooro <pabloalabanza9@gmail.com>
2023-12-28 14:18:00 +0100

    update: import update

--- src/client/components/Auth/User/UserDashboard/UserProfile/Components/UpdatePassword.tsx
+++ src/client/components/Auth/User/UserDashboard/UserProfile/Components/UpdatePassword.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LayoutDashbord from "../../Constants/LayoutDashboard";
+import LayoutDashbord from "../../Extras/LayoutDashboard";
 import UpdatePass from "./InnerComps/UpdatePass";
 
 const UpdatePassword = () => {
