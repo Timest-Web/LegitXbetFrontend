0ba11a0d30873db3e46aabab89a5b4ac46b0374c
Ushie01 <abugodwinaj@gmail.com>
2024-01-16 12:35:08 +0100

    update:mobile auth registration

--- src/client/shared/Drawer/MobileTopDrawer.tsx
+++ src/client/shared/Drawer/MobileTopDrawer.tsx
@@ -16,10 +16,10 @@ const MobileTopDrawer = ({ isOpen, closeDrawer, drawerContent }: Props) => {
 				setOpen={closeDrawer}>
 				<Drawer.Panel
 					position='top'
-					className='bg-white rounded-b-3xl'>
+					className='bg-white rounded-b-3xl overflow-y-auto'>
 					<div className='flex w-full justify-end mb-3 p-4'>
 						<div className='flex items-center justify-center h-8 w-8 rounded-full bg-gray-200'>
-							<ControlsClose onClick={() => closeDrawer(false)}/>
+							<ControlsClose onClick={() => closeDrawer(false)} />
 						</div>
 					</div>
 					<div className='p-3 -mt-10'>{drawerContent}</div>
