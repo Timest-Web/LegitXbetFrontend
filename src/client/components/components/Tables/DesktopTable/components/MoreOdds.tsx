ec88fce0e86dae7b54de29e647086d528207bd49
ushie01 <abugodwinaj@gmail.com>
2024-02-09 17:58:55 +0100

    update: link to more odds

--- src/client/components/components/Tables/DesktopTable/components/MoreOdds.tsx
+++ src/client/components/components/Tables/DesktopTable/components/MoreOdds.tsx
@@ -2,12 +2,13 @@ import RightArrow from "@/src/client/shared/Svg/RightArrow";
 import Link from "next/link";
 
 export const MoreOdds = () => (
-	<Link href='#'
-		className={`flex items-center justify-between text-xs bg-lightAsh w-20 px-2 h-6 rounded-md cursor-pointer`}>
-		<p>All</p>
-		<div className='flex items-center space-x-1'>
-			<p>Odds</p>
-			<RightArrow />
-		</div>
-	</Link>
+  <Link href={`/sports/football/40`}
+    className={`flex items-center justify-between text-xs bg-lightAsh w-20 px-2 h-6 rounded-md cursor-pointer`}
+  >
+    <p>All</p>
+    <div className="flex items-center space-x-1">
+      <p>Odds</p>
+      <RightArrow />
+    </div>
+  </Link>
 );
